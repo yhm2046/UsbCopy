9b0d10e618dfcb823dff863f5ebcf14cdb575fde
Michael <yhm2046@gmail.com>
2021-07-09 14:15:56 +0800

    添加第三方库实现插入拔出usb提示

--- app/src/main/java/com/cnd/zhongkong/usbcopy/MainActivity.java
+++ app/src/main/java/com/cnd/zhongkong/usbcopy/MainActivity.java
@@ -1,14 +1,72 @@
 package com.cnd.zhongkong.usbcopy;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+
+import com.github.mjdev.libaums.UsbMassStorageDevice;
+import com.github.mjdev.libaums.fs.UsbFile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG="MainActivity:xwg";
+//    private UDiskCallBack.OnUDiskCallBack mOnUDiskCallBack = null;
+    private Context mContext;
+    private UsbMassStorageDevice[] storageDevices;
+    private List<UsbFile> usbFiles = new ArrayList<>();
+//    private final UsbManager mUsbManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG,"oncreate..");
+        IntentFilter usbDeviceStateFilter = new IntentFilter();
+        usbDeviceStateFilter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+        usbDeviceStateFilter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        registerReceiver(mUsbReceiver, usbDeviceStateFilter);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"ondestroy..");
+        unregisterReceiver(mUsbReceiver);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+    BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
+                Log.e(TAG,"拔出usb了");
+                UsbDevice device = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                if (device != null) {
+                    Log.e(TAG,"设备的ProductId值为："+device.getProductId());
+                    Log.e(TAG,"设备的VendorId值为："+device.getVendorId());
+                }
+            }else if(UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)){
+                Log.e(TAG,"插入usb了");
+            }
+        }
+    };
 }
